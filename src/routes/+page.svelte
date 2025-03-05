81f9fce220a678a4671ec92cd027fa77f09c579f
Jared Woelfel <jared.woelfel@outlook.com>
2025-03-04 23:46:19 -0600

    add invalidation root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,30 @@
 <script lang="ts">
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	let { data } = $props();
+	let { session, supabase, user } = $derived(data);
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
 <div class="flex min-h-screen items-center justify-center">
 	<div class="flex flex-col">
-		<h2 class="text-xl">Hmmm... have we met before?</h2>
-		<h3 class="text-xl">
-			Please <a href="/auth" class="text-xl font-bold">Login</a> or
-			<a href="/auth" class="text-xl font-bold">Signup</a>
-		</h3>
+		{#if user}
+			<h2 class="text-xl">Hmmm... have we met before?</h2>
+			<h3 class="text-xl">
+				Please <a href="/auth" class="text-xl font-bold">Login</a> or
+				<a href="/auth" class="text-xl font-bold">Signup</a>
+			</h3>
+		{:else}
+			<h2 class="text-xl">Hey, I know you!</h2>
+		{/if}
 	</div>
 </div>
