9e396f45912c7596b46e3f6a938468d2cdb82190
Jared Woelfel <jared.woelfel@outlook.com>
2025-03-06 19:11:22 -0600

    add logout button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,35 +14,42 @@
 
 		return () => data.subscription.unsubscribe();
 	});
+
+	async function signOut() {
+		await supabase.auth.signOut();
+	}
 </script>
 
 <div class="navbar bg-base-100 shadow-sm">
 	<div class="flex-1">
 		<a class="btn btn-ghost text-xl" href="/">Bible Reading Tracker</a>
 	</div>
-	{#if user}
-		<div class="flex-none">
-			<div class="dropdown dropdown-end">
-				<div tabindex="0" role="button" class="btn btn-square btn-ghost">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						class="inline-block h-5 w-5 stroke-current"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-						></path>
-					</svg>
-				</div>
-				<ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box w-52 p-2 shadow">
-					<li><a href="/settings">Settings</a></li>
-				</ul>
+	<div class="flex-none">
+		<div class="dropdown dropdown-end">
+			<div tabindex="0" role="button" class="btn btn-square btn-ghost">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="inline-block h-5 w-5 stroke-current"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+					></path>
+				</svg>
 			</div>
+			<ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box w-52 p-2 shadow">
+				{#if user}
+					<li><a href="/settings">Settings</a></li>
+					<li><button on:click={signOut}>Log out</button></li>
+				{:else}
+					<li><a href="/auth">Log in</a></li>
+				{/if}
+			</ul>
 		</div>
-	{/if}
+	</div>
 </div>
 {@render children()}
